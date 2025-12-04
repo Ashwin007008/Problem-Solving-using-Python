b189e1e7de7153a2528939b9e159fa918e0ba4de
Ashwin007008 <josephashwin477@gmail.com>
2025-12-04 09:55:06 +0530

    Fast and slow pointer

--- Moduel 5/Fast pointer and slow pointer.py
+++ Moduel 5/Fast pointer and slow pointer.py
@@ -1,6 +1,6 @@
 def find_middle_element(arr):  # function to find the middle element of the array
     slow, fast = 0, 0               # initialize two pointers
-    while fast + 2 <= len(arr):  # loop until fast pointer reaches the end
+    while fast + 2 < len(arr):  # loop until fast pointer reaches the end
         slow += 1               # move slow pointer by 1
         fast += 2               # move fast pointer by 2
     return arr[slow]           # return the middle element
